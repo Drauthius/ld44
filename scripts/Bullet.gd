24abd92b479f8ffaa42458ee11b7f8b09f11d58a
Drauthius <albert@diserholt.com>
2019-04-27 15:45:32 +0200

    Basic shooting and animation for movement.

---
+++ scripts/Bullet.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+export var speed : int = 400
+export var lifetime : float = 0.5
+
+func _ready():
+	$Timer.start(lifetime)
+
+func init(color):
+	$Sprite.modulate = color
+
+func _physics_process(delta):
+	position += Vector2(cos(rotation), sin(rotation)) * speed * delta
+
+func _on_Timer_timeout():
+	queue_free()
+	
+func _on_Bullet_body_entered(body):
+	queue_free()
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,10 +1,30 @@
 extends KinematicBody2D
 
-export var speed: int = 300
+export var speed: int = 200
 
-func _physics_process(delta):
-	var dir = Vector2(0, 0)
+var angle = 0
+
+const QPI = PI/4
+
+onready var Bullet = preload("res://scenes/Bullet.tscn")
+
+func _process(_delta):
+	angle = (get_global_mouse_position() - position).angle() + PI
 	
+	if angle > 5*QPI:
+		$AnimationPlayer.play("down")
+	elif angle > 3*QPI:
+		$AnimationPlayer.play("right")
+	elif angle > QPI:
+		$AnimationPlayer.play("up")
+	else:
+		$AnimationPlayer.play("left")
+	$AnimationPlayer.advance(0.01)
+
+func _physics_process(_delta):
+	# Movement
+	var dir = Vector2(0, 0)
+
 	if Input.is_action_pressed("game_down"):
 		dir.y += 1
 	if Input.is_action_pressed("game_up"):
@@ -14,4 +34,17 @@ func _physics_process(delta):
 	if Input.is_action_pressed("game_left"):
 		dir.x -= 1
 	
-	move_and_slide(dir.normalized() * speed)
+	var velocity = move_and_slide(dir.normalized() * speed)
+	
+	if velocity.length_squared() <= 0.001:
+		$AnimationPlayer.stop()
+	else:
+		$AnimationPlayer.play()
+	
+	# Firing
+	if Input.is_action_just_pressed("game_fire"):
+		var bullet = Bullet.instance()
+		bullet.position = -Vector2(20, 0).rotated(angle)
+		bullet.rotation = angle - PI
+		bullet.init(Color("002868"))
+		add_child(bullet)
