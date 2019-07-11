4d191554a5616596b6d3a4c74e29828ee2e4544c
worai <42282410+worai@users.noreply.github.com>
2019-07-11 22:09:36 +0200

    enemy03 timedexplosion first implementation

--- scripts/Enemy03.gd
+++ scripts/Enemy03.gd
@@ -1,19 +1,25 @@
 extends "res://scripts/Enemy.gd"
 
-
+var Explosion = preload("res://scenes/ExplosionTimed.tscn")
 
 func _on_Timer_timeout() -> void:
 	match state:
 		States.SHOOTING: #TODO: dorai!!
 			var angle : float = target.position.angle_to_point(position) + PI
 			var bullet = Bullet.instance()
-			bullet.position = get_global_transform().get_origin() - Vector2(16, 0).rotated(angle)
-			bullet.rotation = angle - PI
-			bullet.init(bullet_modulate)
-			bullet.lifetime = attack_distance.y / float(bullet_speed) * 1.1
-			bullet.speed = bullet_speed
-			$Gun.add_child(bullet)
-			SoundService.call(sound + "_gunshot")
+			if false:
+				bullet.position = get_global_transform().get_origin() - Vector2(16, 0).rotated(angle)
+				bullet.rotation = angle - PI
+				bullet.init(bullet_modulate)
+				bullet.lifetime = attack_distance.y / float(bullet_speed) * 1.1
+				bullet.speed = bullet_speed
+				$Gun.add_child(bullet)
+				SoundService.call(sound + "_gunshot")
+			
+			var explosion = Explosion.instance()
+			explosion.position = target.position
+			$Gun.add_child(explosion)
+			print('SoundService.call(sound + "_electronic_whine")')
 			
 			# Muzzle flash
 			if MuzzleFlash:
--- scripts/LaserFocus.gd
+++ scripts/LaserFocus.gd
@@ -1,12 +1,29 @@
+#belongs to ExplosionArea -> rename from LaserFocus.gd!!
 extends Area2D
 
 export var lifetime : float = 0.35
+export var push : int = 5
+
+var remove = false
 
 func _ready():
+	$Timer.set_wait_time(0.9)
 	$Timer.start()
 	pass 
 
 
-
 func _on_Timer_timeout():
-	pass # Replace with function body.
+	$CollisionShape2D.disabled = false
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	$CollisionShape2D.disabled = true
+	queue_free()
+
+
+func _on_ExplosionArea_body_entered(body):
+	if body.is_in_group("Living"):
+		if not body.is_in_group("Immovable"):
+			# Push back
+			var direction = (position - body.position).normalized()
+			body.position += direction * push
+		body.die()
